26ba3e427954abc7970844efbf264c8d9f59b400
Allison Walke <awalke@ancestry.com>
2017-05-09 13:23:58 -0600

    Starting on Command pattern

---
+++ Phase_0.5/Phase_0.5_Server/command/src/main/java/Commands/ICommand.java
@@ -0,0 +1,7 @@
+package Commands;
+
+/**
+ * Created by awalke on 5/9/17.
+ */
+public interface ICommand {
+}
---
+++ Phase_0.5/Phase_0.5_Server/command/src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by awalke on 5/9/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
